8f51066fb1ddfad3649df1e402f878effaf91437
Akash Srinagesh <akashsrinagesh@gmail.com>
2016-03-15 04:20:55 -0700

    getting location from browswer

--- index.php
+++ index.php
@@ -3,14 +3,30 @@
     <head>
         <title>Pic A Food</title>
         <link href="index.css" type="text/css" rel="stylesheet" />
-        <script src="index.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            navigator.geolocation.getCurrentPosition(GetLocation);
+            function GetLocation(location) {
+                var url = window.location.href;
+                if(!url.includes('lat')) {
+                    location.coords.latitude;
+                    location.coords.longitude;
+                    location.coords.accuracy;
+                    window.location.replace("https://webster.cs.washington.edu/students/akash221/PicFood/index.php?lat=" + location.coords.latitude + "&long=" + location.coords.longitude);
+                }
+            }
+        </script>
     </head>
     <body>
-        <h1>Pic-A-Food</h1>
+        <h1>Pic-A-Food</h1> 
+        <?php 
+            if(!isset($_GET['lat'])) { ?>
+                <p>Loading page</p> 
+            <?php } else { s?>
         <div id="container">
             <?php 
                 include("sample.php");
-                $data = getPics('food','47.6567,-122.31003');
+                $ll = $_GET['lat'].",".$_GET['long'];
+                $data = getPics('food',$ll);
                 foreach($data as $name => $link){ ?>
                     <div title="<?= $name ?>" class="box">
                         <span class="overlay"><?= $name ?></span>
@@ -19,14 +35,6 @@
                 <?php }
             ?>
         </div>
-        
+        <?php } ?>
     </body>
-    <script type="text/javascript">
-        navigator.geolocation.getCurrentPosition(GetLocation);
-        function GetLocation(location) {
-            alert(location.coords.latitude);
-            alert(location.coords.longitude);
-            alert(location.coords.accuracy);
-        }
-    </script>
 </html>

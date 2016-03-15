7cff460f7082d38852900388e786e608290c3528
Charlie Mihran <charlie.mihran@gmail.com>
2016-03-15 15:40:01 -0700

    made loading animation left to right

--- loading.js
+++ loading.js
@@ -1,3 +1,20 @@
 window.onload = function () {
-	$squares = document.querySelectorAll("square");
+	updateAnimation();
+}
+
+window.onresize = function () {
+	updateAnimation();
+}
+
+function updateAnimation() {
+	var squaresInRow = Math.floor(window.innerWidth/220);
+	var squares = document.querySelectorAll(".square");
+	var delay = 0.1;
+	for(var i = 0; i < squares.length; i++){
+		if(i % squaresInRow == 0){
+			delay = 0.1;
+		}
+		squares[i].style.animationDelay= delay + "s";
+		delay += 0.1;
+	}
 }

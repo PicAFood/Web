cebe9bbdb71264824be96681d817484c974b8c47
Charlie Mihran <charlie.mihran@gmail.com>
2016-03-18 15:24:01 -0700

    refactor index.php

--- common.php
+++ common.php
@@ -1,7 +1,35 @@
 <?php
-	function displayHead(){ 
+	function displayHead() { 
 		$home = preg_match("/.*\/PicFood\//", $_SERVER[REQUEST_URI], $matches); 
 		$val = $matches[0]; ?>
 		<h1><a href="<?= "http://$_SERVER[HTTP_HOST]$val"; ?>">Pic-A-Food</a></h1> 
 	<?php }
+
+	function getMainCuisines() {
+		$mainCuisines["American"] = 
+				"http://las-vegas.eat24hours.com/files/cuisines/v4/american.jpg";
+        $mainCuisines["Thai"] = 
+        		"http://www.thaidelightalaska.com/backend/uploads/products/34-banner-20150805103120B5.jpg";
+        $mainCuisines["Vietnamese"] = 
+        		"http://media.crossingtravel.com/files/tag/2015/09/26/image-163.jpg";
+        $mainCuisines["Chinese"] = 
+        		"http://houston.eat24hours.com/files/cuisines/v4/chinese.jpg"; 
+        $mainCuisines["Greek"] = 
+        		"https://img.grouponcdn.com/deal/mNM6qDkg3TZ4Kv4x6gh7/Ua-700x420/v1/c700x420.jpg";
+        $mainCuisines["Indian"] = 
+        		"http://insidenirvana.com/wp-content/uploads/2015/08/best-indian-food-new-orleans.jpg"; 
+        return $mainCuisines;
+	}
+
+	function getSideCuisines() {
+		$sideCuisines["Ice Cream"] = 
+				"http://www.newhealthadvisor.com/images/1HT02932/ice%20cream.jpg"; 
+        $sideCuisines["Bubble Tea"] = 
+        		"http://media1.fdncms.com/thecoast/imager/georges-bubble-tea/u/zoom/4683750/george_sbubbletea1.jpg";
+ 	    $sideCuisines["Pastries"] = 
+ 	    		"http://www.duraneurosandwich.com/wp-content/uploads/2014/05/Pastries-back1-2400.jpg"; 
+        $sideCuisines["Coffee"] = 
+        		"https://vacationidahosprings.files.wordpress.com/2011/07/coffee-cups.jpg";
+        return $sideCuisines;
+	}
 ?>
--- index.php
+++ index.php
@@ -12,38 +12,28 @@
         ?>
         <div id ="container">
           <?php
-            $cuisines["American"] = "http://las-vegas.eat24hours.com/files/cuisines/v4/american.jpg";
-            $cuisines["Thai"] = "http://www.thaidelightalaska.com/backend/uploads/products/34-banner-20150805103120B5.jpg";
-            $cuisines["Vietnamese"] = "http://media.crossingtravel.com/files/tag/2015/09/26/image-163.jpg";
-            $cuisines["Chinese"] = "http://houston.eat24hours.com/files/cuisines/v4/chinese.jpg"; 
-            $cuisines["Greek"] = "https://img.grouponcdn.com/deal/mNM6qDkg3TZ4Kv4x6gh7/Ua-700x420/v1/c700x420.jpg";
-            $cuisines["Indian"] = "http://insidenirvana.com/wp-content/uploads/2015/08/best-indian-food-new-orleans.jpg"; 
-
-            ?>
-            <div id="cuisines">
-            <?php foreach($cuisines as $name => $link) { ?>
+            $mainCuisines = getMainCuisines();
+          ?>
+          <div id="cuisines">
+            <?php foreach($mainCuisines as $name => $link) { ?>
                 <div class="square box" onclick="buisnessSelected(this.id)" id="<?= $name ?>">
                     <span class="overlay"><?= trim($name) ?></span>
                     <div class="bg" style="background-image: url(<?= $link ?>);"></div>
                 </div>
             <?php } ?>
-            </div>
+          </div>
 
-            <?php
-              $cuisines = "";
-              $cuisines["Ice Cream"] = "http://www.newhealthadvisor.com/images/1HT02932/ice%20cream.jpg"; 
-              $cuisines["Bubble Tea"] = "http://media1.fdncms.com/thecoast/imager/georges-bubble-tea/u/zoom/4683750/george_sbubbletea1.jpg";
-              $cuisines["Pastries"] = "http://www.duraneurosandwich.com/wp-content/uploads/2014/05/Pastries-back1-2400.jpg"; 
-              $cuisines["Coffee"] = "https://vacationidahosprings.files.wordpress.com/2011/07/coffee-cups.jpg";
-            ?>
-            <div id="cuisines">
-            <?php foreach($cuisines as $name => $link) { ?>
-                <div class="square box" onclick="buisnessSelected(this.id)" id="<?=$name?>">
+          <?php
+            $sideCuisines = getSideCuisines();
+          ?>
+          <div id="cuisines">
+            <?php foreach($sideCuisines as $name => $link) { ?>
+                <div class="square box" onclick="buisnessSelected(this.id)" id="<?= $name ?>">
                     <span class="overlay"><?= trim($name) ?></span>
                     <div class="bg" style="background-image: url(<?= $link ?>);"></div>
                 </div>
             <?php } ?>
-            </div>
+          </div>
 
           <div class="sk-cube-grid" style="display: none;">
             <?php

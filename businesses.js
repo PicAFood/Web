b9c3fe331dea58b62449f1479a93ba4d235971b0
Charlie Mihran <charlie.mihran@gmail.com>
2016-03-18 18:06:56 -0700

    initial steps on setting on clicking on a business

---
+++ businesses.js
@@ -0,0 +1,6 @@
+function viewBusiness(var id){
+	// so ideally ID would be the actual ID of the business,
+	// assuming thats what the Yelp API wants. Not sure how
+	// to go about getting this, probably have to modify getPics
+	// to also return ID, since it only returns name and url now
+}
--- foodgrid.php
+++ foodgrid.php
@@ -3,6 +3,7 @@
     <head>
         <title>Pic A Food</title>
         <link href="index.css" type="text/css" rel="stylesheet" />
+        <script src="businesses.js" type="text/javascript"></script>
     </head>
     <body>
         <?php
@@ -16,7 +17,7 @@
                 $type = $_GET['type'];
                 $data = getPics($type,$ll);
                 foreach($data as $name => $link){ ?>
-                    <div class="square box" onclick="viewBusiness(this)"style="display: inline-block">
+                    <div class="square box" onclick="viewBusiness(this.id)" style="display: inline-block" id="<?=$name?>">
                         <span class="overlay"><?= trim($name) ?></span>
                         <div class="bg" style="background-image: url(<?= $link ?>);"></div>
                     </div>

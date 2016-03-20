2f867140d3d604582e2e0f710b9717406e048e47
Charlie Mihran <charlie.mihran@gmail.com>
2016-03-20 00:55:23 -0700

    fix js bug

--- businesses.js
+++ businesses.js
@@ -1,4 +1,4 @@
-function viewBusiness(var id){
+function viewBusiness(id){
 	// so ideally ID would be the actual ID of the business,
 	// assuming thats what the Yelp API wants. Not sure how
 	// to go about getting this, probably have to modify getPics
